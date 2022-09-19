917b8009bd4fb46575d4a0f92ffbc79244e719c4
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-19 20:37:15 +0530

    Add files via upload

---
+++ PerfectNumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int s,sum;
+	cout<<"Given number: ";
+	cin>> s;
+	sum=0;
+	for(int i = 1; i <s;i++){
+        if(s % i == 0)
+            sum = sum + i;
+    }
+    
+    if(sum == s)
+        cout << s <<" is a perfect number";
+    else
+        cout << s <<" is not a perfect number";
+    
+
+
+
+
+   
+	
+}
