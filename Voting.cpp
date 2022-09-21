e1d65932310003e378b52284557548193c3f0979
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-21 10:17:33 +0530

    Add files via upload

---
+++ Voting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int Age;
+	cout<<"\nEnter your age : ";
+	cin>>Age;
+
+  if(Age >= 18)
+    cout<<"Eligible for Voting! ";
+  else
+    cout<<"Not eligible for Voting! ";
+
+
+	return 0;
+}
